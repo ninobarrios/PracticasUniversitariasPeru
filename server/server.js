6dfa6f9c6beea4799b5df174f5d196d09fa514cd
NinoBarrios <ninobbarrios.bellido@gmail.com>
2025-01-30 19:45:38 -0500

    agregando proyecto

---
+++ server/server.js
@@ -0,0 +1,246 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const path = require('path');
+const compression = require('compression');
+const NodeCache = require('node-cache');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const cache = new NodeCache({ stdTTL: 600 });
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, '../client/build'), { maxAge: '1d' }));
+app.use(cors());
+app.use(express.json());
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: 10000,
+    ssl: { rejectUnauthorized: false },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+    connection.release();
+});
+
+app.get("/ofertas-laborales", (req, res) => {
+    const query = `
+        SELECT plataforma, nom_oferta, nom_empresa, lugar, link_pagina 
+        FROM ofertas_laborales 
+        ORDER BY fecha DESC, RAND();
+    `;
+    pool.query(query, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send("Error al obtener ofertas");
+        }
+        res.json(results);
+    });
+});
+
+app.get("/ofertas-laborales-hoy", (req, res) => {
+    const cacheKey = "ofertas-laborales-hoy";
+    const cachedResults = cache.get(cacheKey);
+
+    if (cachedResults) return res.json(cachedResults);
+
+    const query = `
+SELECT plataforma, nom_oferta, nom_empresa, lugar, link_pagina FROM ofertas_laborales WHERE fecha = (SELECT MAX(fecha) FROM ofertas_laborales) ORDER BY nom_empresa DESC;
+
+    `;
+
+    pool.query(query, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send("Error al obtener ofertas");
+        }
+        cache.set(cacheKey, results);
+        res.json(results);
+    });
+});
+
+app.get('/sugerencias', (req, res) => {
+    const palabra = req.query.palabra || '';
+
+    if (palabra.length === 0) {
+        return res.json([]);
+    }
+
+    const sql = `
+        SELECT DISTINCT nom_oferta 
+        FROM ofertas_laborales 
+        WHERE nom_oferta COLLATE utf8mb4_unicode_ci LIKE ? 
+        LIMIT 10;
+    `;
+
+    pool.query(sql, [`%${palabra}%`], (error, results) => {
+        if (error) {
+            console.error('Error ejecutando la consulta de sugerencias:', error);
+            return res.status(500).json({ error: 'Error al obtener sugerencias' });
+        }
+        const sugerencias = results.map(row => row.nom_oferta);
+        res.json(sugerencias);
+    });
+});
+app.get('/sugerencias2', (req, res) => {
+    const palabra = req.query.palabra || '';
+
+    if (palabra.length === 0) {
+        return res.json([]);
+    }
+
+    const sql = `
+SELECT DISTINCT nom_oferta 
+FROM ofertas_laborales 
+WHERE fecha_insercion = (
+    SELECT MAX(fecha_insercion) 
+    FROM ofertas_laborales
+) 
+  AND nom_oferta COLLATE utf8mb4_unicode_ci LIKE ? 
+LIMIT 10;
+
+    `;
+
+    pool.query(sql, [`%${palabra}%`], (error, results) => {
+        if (error) {
+            console.error('Error ejecutando la consulta de sugerencias:', error);
+            return res.status(500).json({ error: 'Error al obtener sugerencias' });
+        }
+        const sugerencias = results.map(row => row.nom_oferta);
+        res.json(sugerencias);
+    });
+});
+app.get('/contarObservacionesDiaAnterior', (req, res) => {
+    const cacheKey = "contarObservacionesDiaAnterior";
+    const cachedResults = cache.get(cacheKey);
+
+    if (cachedResults) return res.json(cachedResults);
+
+    const query = `
+        SELECT COUNT(*) AS count 
+        FROM ofertas_laborales 
+        WHERE DATE(fecha) = (SELECT DATE(MAX(fecha)) FROM ofertas_laborales);
+    `;
+
+    pool.query(query, (err, results) => {
+        if (err) {
+            console.error('Error ejecutando la consulta:', err);
+            return res.status(500).send("Error al obtener datos");
+        }
+        cache.set(cacheKey, results[0]);
+        res.json(results[0]);
+    });
+});
+
+app.get('/contarObservacionesSemana', (req, res) => {
+    const cacheKey = "contarObservacionesSemana";
+    const cachedResults = cache.get(cacheKey);
+
+    if (cachedResults) return res.json(cachedResults);
+
+    const query = `
+        SELECT COUNT(*) AS count 
+        FROM ofertas_laborales AS ol 
+        JOIN (
+            SELECT fecha 
+            FROM ofertas_laborales 
+            GROUP BY fecha 
+            ORDER BY fecha DESC 
+            LIMIT 7
+        ) AS ultimas_fechas 
+        ON ol.fecha = ultimas_fechas.fecha;
+    `;
+
+    pool.query(query, (err, results) => {
+        if (err) {
+            console.error('Error ejecutando la consulta:', err);
+            return res.status(500).send("Error al contar observaciones de la semana");
+        }
+
+        cache.set(cacheKey, results[0]);
+        res.json(results[0]);
+    });
+});
+
+app.get('/contarObservacionesTotal', (req, res) => {
+    const cacheKey = "contarObservacionesTotal";
+    const cachedResults = cache.get(cacheKey);
+
+    if (cachedResults) return res.json(cachedResults);
+
+    const query = `SELECT COUNT(*) AS count FROM ofertas_laborales;`;
+    pool.query(query, (err, results) => {
+        if (err) {
+            console.error('Error ejecutando la consulta:', err);
+            return res.status(500).send("Error al contar observaciones totales");
+        }
+        cache.set(cacheKey, results[0]);
+        res.json(results[0]);
+    });
+});
+
+app.get("/selecionardepartamento/:departamento", (req, res) => {
+    const departamento = req.params.departamento;
+    const query = `
+        SELECT plataforma, nom_oferta, nom_empresa, lugar, link_pagina 
+        FROM ofertas_laborales 
+        WHERE lugar LIKE ? 
+        ORDER BY fecha DESC, RAND(); 
+    `;
+    const values = [`%${departamento}%`];
+
+    pool.query(query, values, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send("Error al obtener ofertas");
+        }
+        res.json(results);
+    });
+});
+
+app.get("/selecionarcarrera/:carrera", (req, res) => {
+    const carrera = req.params.carrera;
+    const query = `
+        SELECT plataforma, nom_oferta, nom_empresa, lugar, link_pagina 
+        FROM ofertas_laborales 
+        WHERE nom_oferta REGEXP ? 
+        ORDER BY fecha DESC, RAND();
+    `;
+
+    pool.query(query, [carrera], (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send("Error al obtener ofertas");
+        }
+        res.json(results);
+    });
+});
+
+app.use(express.static(path.join(__dirname, '../wwwroot')));
+
+// Ruta para sitemap.xml
+app.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.join(__dirname, '../wwwroot/sitemap.xml'));
+});
+
+// Ruta para el index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../wwwroot/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ wwwroot/scripts.js
@@ -0,0 +1,195 @@
+function obtenerEstadisticas() {
+    axios.all([
+        axios.get('https://www.practicasuniversitariasperu.com/contarObservacionesSemana'),
+        axios.get('https://www.practicasuniversitariasperu.com/contarObservacionesTotal'),
+        axios.get('https://www.practicasuniversitariasperu.com/contarObservacionesDiaAnterior')
+    ])
+        .then(axios.spread((responseSemana, responseTotal, responseDiaAnterior) => {
+            console.log("Semana:", responseSemana.data);
+            console.log("Total:", responseTotal.data);
+            console.log("Día Anterior:", responseDiaAnterior.data);
+
+            const conteo = responseDiaAnterior.data?.count || 0;
+            const conteo2 = responseSemana.data?.count || 0;
+            const conteo3 = responseTotal.data?.count || 0;
+
+            document.getElementById('spinner').style.display = 'none';
+
+            document.querySelector('.total_observaciones .dia p').textContent = conteo.toLocaleString('es-PE');
+            document.querySelector('.total_observaciones .semana p').textContent = conteo2.toLocaleString('es-PE');
+            document.querySelector('.total_observaciones .total p').textContent = conteo3.toLocaleString('es-PE');
+        }))
+        .catch(error => {
+            document.getElementById('spinner').style.display = 'none';
+            console.error('Error al obtener las estadísticas:', error);
+            document.getElementById('stats-container').innerHTML = '<p class="error-message">Error al cargar las estadísticas.</p>';
+        });
+}
+
+document.addEventListener("DOMContentLoaded", obtenerEstadisticas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let currentPage = 1; 
+const offersPerPage = 20; 
+let offersData = []; 
+
+window.onload = function () {
+    obtenerEstadisticas();
+
+    document.getElementById('spinner').style.display = 'block';
+
+    axios.get('https://www.practicasuniversitariasperu.com/ofertas-laborales-hoy')
+        .then(response => {
+            document.getElementById('spinner').style.display = 'none';
+
+            if (response.data && Array.isArray(response.data) && response.data.length > 0) {
+                offersData = response.data; 
+                displayOffers(); 
+            } else {
+                document.getElementById('offers-container2').innerHTML = '<p>No hay ofertas disponibles.</p>';
+            }
+        })
+        .catch(error => {
+            document.getElementById('spinner').style.display = 'none';
+            console.error('Error al obtener las ofertas:', error);
+            document.getElementById('offers-container2').innerHTML = '<p class="error-message">Error al cargar las ofertas.</p>';
+        });
+};
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getPlatformImage(platform) {
+    const platformImages = {
+        'Bumeran': '/imagenes/bumeran.avif',
+        'Computrabajo': '/imagenes/computrabajo.avif',
+        'Linkedin': '/imagenes/linkedin.avif',
+        'Indeed': '/imagenes/indeed.avif',
+        'Buscojobs': '/imagenes/buscojobs.avif',
+        'Convocatorias de Trabajo': '/imagenes/convocatorias_2024.avif',
+
+
+    };
+    return platformImages[platform] || null;
+}
+
+function displayOffers() {
+    const start = (currentPage - 1) * offersPerPage;
+    const end = currentPage * offersPerPage;
+    const offersToDisplay = offersData.slice(start, end); 
+
+    let htmlContent = '';
+    offersToDisplay.forEach(offer => {
+        const platformImage = getPlatformImage(offer.plataforma);
+
+        htmlContent += `
+            <div class="boxcajaoferta">
+                <h2>${capitalizeFirstLetter(offer.nom_oferta)}</h2>
+                <div class="division"></div>
+                <div class="empresa">
+                    <h3>${capitalizeFirstLetter(offer.nom_empresa)}</h3>
+                </div>
+                <div class="lugar">
+                    <img src="/imagenes/ubi.avif" alt="Ubicación" />
+                    <h3>${offer.lugar}</h3>
+                </div>
+                <div class="foto_plataforma">
+                    ${platformImage ? `<img src="${platformImage}" alt="Logo de ${offer.plataforma}" />` : ''}
+                </div>
+                
+            </div>
+        `;
+    });
+
+    document.getElementById('offers-container2').innerHTML = htmlContent;
+    updatePaginationButtons(); 
+    displayPageNumbers(); 
+}
+
+function updatePaginationButtons() {
+    const totalPages = Math.ceil(offersData.length / offersPerPage); 
+
+    document.getElementById('prevPage').disabled = currentPage === 1;
+
+    document.getElementById('nextPage').disabled = currentPage === totalPages;
+}
+
+function displayPageNumbers() {
+    const totalPages = Math.ceil(offersData.length / offersPerPage);
+    const maxPagesToShow = 5; 
+    let startPage = currentPage - Math.floor(maxPagesToShow / 2);
+    let endPage = currentPage + Math.floor(maxPagesToShow / 2);
+
+    if (startPage < 1) {
+        startPage = 1;
+        endPage = Math.min(maxPagesToShow, totalPages);
+    }
+
+    if (endPage > totalPages) {
+        endPage = totalPages;
+        startPage = Math.max(1, totalPages - maxPagesToShow + 1);
+    }
+
+    let pageNumbersHTML = '';
+
+    for (let i = startPage; i <= endPage; i++) {
+        pageNumbersHTML += `
+            <span class="pageNumber ${i === currentPage ? 'active' : ''}" onclick="goToPage(${i})">${i}</span>
+        `;
+    }
+
+    document.getElementById('pageNumbers').innerHTML = pageNumbersHTML;
+}
+
+function changePage(direction) {
+    const totalPages = Math.ceil(offersData.length / offersPerPage);
+
+    if (direction === 'next' && currentPage < totalPages) {
+        currentPage++;
+    } else if (direction === 'prev' && currentPage > 1) {
+        currentPage--; 
+    }
+
+    displayOffers(); 
+}
+
+function goToPage(pageNumber) {
+    currentPage = pageNumber;
+    displayOffers(); 
+}
+
+let isMenuOpen = false;
+
+        function toggleMenu() {
+            isMenuOpen = !isMenuOpen;
+            const navbarCollapse = document.getElementById('navbarNav');
+            navbarCollapse.classList.toggle('show', isMenuOpen);
+        }
+
+        // Función para manejar clics en los enlaces del menú
+        function handleNavItemClick() {
+            if (isMenuOpen) {
+                toggleMenu(); // Cerrar el menú si se hace clic en un enlace
+            }
+        }
